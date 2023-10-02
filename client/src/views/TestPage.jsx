e0c9a192e391558d73e95f2b9b1a47b67288781c
DarkerYetDarker <89354153+DarkerYetDarker@users.noreply.github.com>
2023-10-02 23:15:22 +0000

    added more functionality

--- client/src/views/TestPage.jsx
+++ client/src/views/TestPage.jsx
@@ -3,8 +3,44 @@ import * as React from "react"
 
 
 const handleClick = () =>{
-	alert('The info gather');
 	window.location.href= 'https://en.wikipedia.org/wiki/Train';
+};
+
+function calculate(first, oper, last) {
+	let result;
+	if(oper.value == "+"){
+		result = first.value + last.value;
+	}
+	else if(oper.value == "-"){
+		result = first.value - last.value;
+	}
+	else if(oper.value == "/"){
+		result = first.value / last.value;
+	}
+	else if(oper == "*"){
+		result = first.value * last.value;
+	}
+	else{
+		return -1;
+	} 
+	
+	return <h1>result</h1>;
+}
+
+const nicholasContainer = () => {
+	return (
+		<div>
+			<h1 align='center'>Nicholas Sternecker</h1>
+			<p>
+				There is no way a bee should be able to fly.<br />
+				Its wings are too small to get its fat little body off the ground.<br />
+				The bee, of course, flies anyway<br />
+				because bees don't care what humans think is<br />
+				Impossible<br />
+			</p>
+		</div>
+
+	);
 };
 
 const nathanClick = () => {
@@ -19,26 +55,30 @@ const nathanContainer = () => {
 			<p>This is the section for Nathan Herscovici, one of the members of the RAAT team</p>
 		</div> 
 	);
-
+};
 
 const TestPage = () => {
 	return (
 		<div>
 			<div>
-			</div>
-			<div> 
-			</div>
-			<div> 
-			</div>
-			<div>
+				{nicholasContainer()}
 				<button OnClick={handleClick}>Nicholas button</button>
+				<label for="val1">Value 1:</label>
+				<input type="number" id="val1" name="val1"></input>
+				<label for="op"> Operator</label>
+				<select name="op" id="op">
+					<option value="+">+</option>
+					<option value="-">-</option>
+					<option value="/">/</option>
+					<option value="*">*</option>
+				</select>
+				<label for="val2"> Value 2:</label>
+				<input type="number" id="val2" name="val2"></input>
+				<button onClick={calculate(val1, op, val2)}>Calculate</button><br/>
+			</div>
 			<div>
 				{nathanContainer()}
 				<button type="button" onClick={nathanClick}>Don't do it</button>
-			</div>
-			<div>
-			</div>
-			<div>
 			</div>
 		</div>
 	);
