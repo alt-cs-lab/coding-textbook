acede7e27840eeb248b07b5a209355536487d5c1
Gibo-Will02 <73460098+Gibo-Will02@users.noreply.github.com>
2023-10-04 14:58:54 +0000

    Created component that acts as a checklist.

--- client/src/views/TestPage.jsx
+++ client/src/views/TestPage.jsx
@@ -1,15 +1,47 @@
 import * as React from "react"
 
+const GibsonComponent = () => {
+	// State with list of all checked item
+	const [checked, setChecked] = React.useState([]);
+	const checkList = ["Reading 1", "Reading 2", "Reading 3", "Reading 4"];
+  
+	// Add/Remove checked item from list
+	const handleCheck = (event) => {
+		var updatedList = [...checked]; //Getting the current list of checked items
+		if (event.target.checked) {
+			updatedList = [...checked, event.target.value];
+		}
+		setChecked(updatedList);
+	};
+  
+	// Return classes based on whether item is checked
+	// Is used alongside .css file to strike through text
+	var isChecked = (item) =>
+	  	checked.includes(item) ? "checked-item" : "not-checked-item";
+  
+	return (
+		<div className="GibsonComponent">
+			<div className="checkList">
+			<div className="title">Your CheckList:</div>
+			<div className="list-container">
+				{checkList.map((item, index) => (
+				<div key={index}>
+					<input value={item} type="checkbox" onChange={handleCheck} />
+					<span className={isChecked(item)}>{item}</span>
+				</div>
+				))}
+			</div>
+			</div>
+		</div>
+	);
+}
+
 const TestPage = () => {
 	return (
 		<div>
-			<div> Gibson
-			</div>
-			<div> Nathan
-			</div>
-			<div> Nate
-			</div>
-			<div> Nicholas
+			<div>
+				<h1>Gibson's Portion</h1>
+				<GibsonComponent />
 			</div>
 		</div>
 	);
