738707caf4ed2c2a590c50dbcd7b4beb866bffa5
DarkerYetDarker <89354153+DarkerYetDarker@users.noreply.github.com>
2023-10-04 16:15:36 +0000

    current work

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,3 +6,18 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('text is rendered', () => {
+  expect(screen.getByText('There is no way a bee should be able to fly.')).toBeInTheDocument()
+  expect(screen.getByText('Its wings are too small to get its fat little body off the ground.')).toBeInTheDocument()
+  expect(screen.getByText('The bee, of course, flies anyway')).toBeInTheDocument()
+  expect(screen.getByText('because bees')).toBeInTheDocument()
+  expect(screen.getByText('Impossible')).toBeInTheDocument()
+})
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Link page="https://en.wikipedia.org/wiki/Train">Facebook</Link>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- client/testing/TestPage.test.js
+++ client/testing/TestPage.test.js
@@ -1,1 +1,4 @@
 import renderer from 'react-test-renderer';
+import App from './App';
+import TestPage from '../src/views/TestPage';
+
