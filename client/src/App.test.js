e42cf4be0b2beeb832ec83e28716223726641000
nlillich <89368743+nlillich@users.noreply.github.com>
2023-10-06 16:00:20 +0000

    Added tests for component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import ButtonTest from './views/TestPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const buttontypes = ['one', 'two', 'three'];
+
+test('ButtonTest component renders and toggles buttons correctly', () => {
+  const { getByText } = render(<ButtonTest buttontypes={buttontypes} />);
+  
+  // sets first button active
+  const activeButton1 = getByText('one');
+  expect(activeButton1).toHaveStyle('opacity: 1');
+  // tests second button is initially inactive
+  const inactiveButton2 = getByText('two');
+  expect(inactiveButton2).toHaveStyle('opacity: 0.6');
+
+  // clicks second button
+  fireEvent.click(inactiveButton2);
+
+  // tests when the second button is clicked
+  expect(activeButton1).toHaveStyle('opacity: 0.6');
+  expect(inactiveButton2).toHaveStyle('opacity: 1');
 });
