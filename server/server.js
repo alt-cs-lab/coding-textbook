1656821425296c4c960c963b25370392b2ea26c4
NathanHerscovici <54340741+NathanHerscovici@users.noreply.github.com>
2023-10-18 16:23:25 +0000

    try another browser

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require("./src/app")
 
 app.listen( 3050, () => console.log("Listening on port 3050"))
--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,36 @@ app.get('/api/assigned-readings', (req, res) => {
   })
 })
 
+app.get('/api/lti', (req, res) => {
+  res.status(200).json({
+    0: {id: "HELLO"}
+  })
+  console.log("SENT LTI STUFF")
+})
+
+// If we are serving our app through a proxy server,
+// the proxy server may be using HTTPS protocol while
+// the express server is using HTTPS.  This will
+// cause the OAuth signatures to not match.  Setting 
+// a proxy trust setting on express will have it 
+// reflect the protocol used by the proxy instead.
+var trustProxy = process.env.TRUST_PROXY;
+if(trustProxy) {
+  // The 'trust proxy' setting can either be a boolean
+  // (blanket trust any proxy) or a specific ip address
+  if(trustProxy === "true") app.set('trust proxy', true);
+  else app.set('trust proxy', trustProxy);
+}
+
+// We use the bodyparser middleware to process incoming
+// request bodies
+app.use(bodyParser.urlencoded({extended: false}));
+
+// For this app we have a single route that responds to
+// LTI launch requests. 
+//app.post('/api/lti', require('./middleware/verify-lti-launch'), require('./endpoints/lti-launch'));
+
+
 app.post('/api/reading', async (req, res) => {
   const response = await axios.get(req.body.url);
   res.send(response.data.replace('</body>', '<script src="https://hypothes.is/embed.js" async></script></body>'))
