1e99952919d03628a02a55c012ada78654dd7f8c
Gibo-Will02 <73460098+Gibo-Will02@users.noreply.github.com>
2023-11-06 16:40:36 +0000

    Removed reference to LTI page and Assigned Readings page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import TestPage from './views/TestPage';
 import Navbar from './components/NavBar/index';
-import LTIPage from './views/LTIPage';
 
-import AssignedReadingView from './views/AssignedReadingView'
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import { useEffect } from 'react';
@@ -15,9 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<AssignedReadingView />} />
         <Route path='/testpage' element={<TestPage />} />
-        <Route path='/LTIPage' element={<LTIPage />} />
       </Routes>
     </Router>
   );
