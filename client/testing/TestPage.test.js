2bac0ccc354548c42cb535314eb38640ee018ef5
NathanHerscovici <54340741+NathanHerscovici@users.noreply.github.com>
2023-11-17 11:48:46 -0600

    Update TestPage.test.js

---
+++ client/testing/TestPage.test.js
@@ -0,0 +1,2 @@
+//We were unable to add testing this sprint as there were numerous errors during the coding cycle. The plan is to implement them in the next sprint, and has been 
+//approved by Professor Bean.
